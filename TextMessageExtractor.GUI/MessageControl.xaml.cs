8088f1b5d5bf9225d3cb2746ae68fcd6a3abafe4
Roland Munsil <roland@munsil.com>
2017-08-17 15:44:31 +0800

    Added GUI proj with some initial testing stuff

---
+++ TextMessageExtractor.GUI/MessageControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextMessageExtractor.GUI
+{
+    /// <summary>
+    /// Interaction logic for Message.xaml
+    /// </summary>
+    public partial class MessageControl : UserControl
+    {
+        public MessageControl(Message message)
+        {
+            InitializeComponent();
+
+            this.Background = message.incoming ? Brushes.LightBlue : Brushes.DarkBlue;
+            this.HorizontalAlignment = message.incoming ? HorizontalAlignment.Left : HorizontalAlignment.Right;
+
+            if(message.msgType == Message.MessageType.SMS)
+            {
+                stackPanel.Children.Add(new TextBlock()
+                {
+                    Text = message.body,
+                    Foreground = Brushes.White,
+                    TextWrapping = TextWrapping.Wrap
+                });
+            }
+            else
+            {
+                foreach (Message.Attachment attachment in message.attachments)
+                {
+                    if (attachment.contentType == "text/plain")
+                    {
+                        stackPanel.Children.Add(new TextBlock()
+                        {
+                            Text = attachment.DataAsText,
+                            Foreground = Brushes.White,
+                            TextWrapping = TextWrapping.Wrap
+                        });
+                    }
+                    else if (attachment.contentType.StartsWith("image/"))
+                    {
+                        BitmapImage src = new BitmapImage()
+                        {
+                            StreamSource = new MemoryStream(attachment.data)
+                        };
+
+                        stackPanel.Children.Add(new Image()
+                        {
+                            Source = src
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TextMessageExtractor.GUI/MessageViewer.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using TextMessageExtractor;
+
+namespace TextMessageExtractor.GUI
+{
+    /// <summary>
+    /// Interaction logic for MessageViewer.xaml
+    /// </summary>
+    public partial class MessageViewer : UserControl
+    {
+        public MessageViewer()
+        {
+            InitializeComponent();
+
+            Importer importer = new Importer(@"C:\Users\rolan\OneDrive\Code\VS2017 Projects\TextMessageExtractor\TextMessageExtractor.GUI\bin\Debug\Backup copy", PhoneNumberNormalizers.UnitedStates);
+            MessageDatabase messageDB = importer.ImportMessages();
+            Conversation conversation = messageDB.GetConversations()[0];
+
+            foreach(Message message in conversation)
+            {
+                stackPanel.Children.Add(new MessageControl(message) { Width = this.Width * 3.0 / 4.0 });
+            }
+        }
+    }
+}
